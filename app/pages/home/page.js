2f36adaddcfd35b2ac43ec2e4287762be10db3c9
ejmudrak <ejmudrak@gmail.com>
2018-01-21 18:48:15 -0500

    Minor UI fixes

--- app/pages/home/page.js
+++ app/pages/home/page.js
@@ -28,6 +28,41 @@ import Logo from '../../assets/logo.png';
 // import Record from './record.png';
 // import Logo from './logo.png';
 
+const records = [
+  {
+    "album": "Coloring Book",
+    "artist": "Chance The Rapper",
+    "image": "../../assets/chance.png",
+    "tracks": [ "One","Two","Three","Four"],
+    "notes": "This album is so happy!"
+  },
+  {
+    "album": "Late Registration",
+    "artist": "Kanye West",
+    "image": "../../assets/kanye.jpg",
+    "tracks": [ "One","Two","Three","Four"],
+    "notes": "This album is very good!"
+  },
+
+];
+
+
+/* IDEAS & TO-DO:
+    + Top 5, Top Tier, and Other Favorites bins:
+        + Show all album art at once for Top 5
+        + Flip through Top Tier and Other Favorites
+    + 3D Transform record bin, like the hip-hop Google Doodle
+    + Liner notes page:
+        + Tracklist
+        + Story
+        + Music player
+    + Controls UI on record bin:
+        + Icon for albums with liner notes
+        + Link to more info about band/albums
+        + Arrows for flipping through albums
+    + Use JS map fn for JSON albums to displayed
+*/
+
 
 /* -------------------- Home - MAIN COMPONENT ----------------------- */
 
@@ -76,20 +111,61 @@ class AppBar extends React.Component {
 
 class Bin extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+        open: false
+    }
+  }
+
+  openAlbum = (data) => {
+    this.setState({ open: true });
+  }
+
+
   render () {
     return(
       <div className={styles.binContainer} >
-        <div className={styles.binFront}>
+          <div className={styles.binFront}></div>
+          <div className={styles.binBack}>
+            <div className={styles.records}>
+                <Album className={styles.album} image={records[0].image} />
+                <Album className={styles.album} image={records[1].image} />
+                <img className={styles.album} src="../../assets/paak.png" />
+                <img className={styles.album} src="../../assets/frank.jpg" />
+                <img className={styles.album} src="../../assets/glass_animals.jpg" />
+            </div>
         </div>
-        <div className={styles.binBack}>
-          <div className={styles.records}>
-            <img className={styles.album} src="../../assets/chance.png" />
-            <img className={styles.album} src="../../assets/kanye.jpg" />
-            <img className={styles.album} src="../../assets/paak.png" />
-            <img className={styles.album} src="../../assets/frank.jpg" />
-            <img className={styles.album} src="../../assets/glass_animals.jpg" />
-          </div>
+        {/* <LinerNotes className={styles.currentAlbumContainer} openAlbum={this.openAlbum}/> */}
+      </div>
+    );
+  }
+}
+
+class Album extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    }
+  }
+
+  handleOpen = () => this.setState({ open: true });
+  handleClose = () => this.setState({ open: false });
+
+// conditionally render small or full screen view of album
+
+  render () {
+    const albumOpen = this.state.open;
+    return (
+      <div>
+      { albumOpen ? (
+        <div className={styles.currentAlbumContainer}>
+          <button className={styles.closeButton} onTouchTap={this.handleClose}>Close</button>
         </div>
+      ) : (
+        <img className={styles.album} src={this.props.image} onTouchTap={this.handleOpen}/>
+    )}
       </div>
     );
   }
